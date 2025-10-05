4383dcc74edd8913f85b593cd7b7d7a9c55a862e
Kevin K <npy16@txstate.edu>
2025-10-05 16:38:40 -0500

    Misc minor adjustments

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -7,6 +7,6 @@ import java.util.ArrayList;
 public class Player {
     //Private attributes
     private String color;
-    private ArrayList<Piece> piecesLeft
+    private ArrayList<Piece> piecesLeft;
 
 }
