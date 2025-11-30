ec89e82d8517bd5e2b1ced5cfdb870d6424d7aaa
Kevin K <npy16@txstate.edu>
2025-11-30 16:14:44 -0600

    Put the public class Chess back into the main class for jdk 24 compatibility, re-generated the javadoc for good measure

--- src/Chess.java
+++ src/Chess.java
@@ -3,6 +3,8 @@ import GUI.GUIBoard;
 /**
  * Main class of the project. Calls GUIBoard
  */
-public static void main(String[] args) {
-    new GUIBoard();
+public class Chess{
+    public static void main(String[] args) {
+        new GUIBoard();
+    }
 }
